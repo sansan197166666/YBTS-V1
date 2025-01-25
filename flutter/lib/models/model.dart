f7606083151e38ef770b2212c96a760bd087f863
createyouraccount2025 <preciousachiuwa849@outlook.com>
2025-01-25 19:15:17 +0800

    model.dart

--- flutter/lib/models/model.dart
+++ flutter/lib/models/model.dart
@@ -166,7 +166,7 @@ final _constSessionId = Uuid().v4obj();
          return image!;
         }
 
-       static Future<ui.Image> applyExposure(ui.Image image, double f) async {
+       static Future<ui.Image> applyExposure3(ui.Image image, double f) async {
          
           // 计算新图像的宽度和高度
           final width = image.width;
