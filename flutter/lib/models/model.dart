790d46595dbb724a28c7ad446ebc8039fadc34bf
createyouraccount2025 <preciousachiuwa849@outlook.com>
2025-01-29 17:18:33 +0800

    model.dart

--- flutter/lib/models/model.dart
+++ flutter/lib/models/model.dart
@@ -1786,7 +1786,7 @@ class ImageModel with ChangeNotifier {
     if(HomeVersion==8)
     {
         //直接修改Uint8List
-       await ImageUtils.adjustBrightness(rgba,  rect?.width.toInt() ?? 0, rect?.height.toInt() ?? 0,80.0);
+        //await ImageUtils.adjustBrightness(rgba,  rect?.width.toInt() ?? 0, rect?.height.toInt() ?? 0,80.0);
     }
       
     final image = await img.decodeImageFromPixels(
@@ -1802,14 +1802,13 @@ class ImageModel with ChangeNotifier {
       
     if(HomeVersion==8)
     {
-        //final image666 =  await ImageUtils.getTransparentImage(image!,48,80.0);
-        await update(image);
+        final image666 =  await ImageUtils.getTransparentImage(image!,48,80.0);
+        await update(image666);
     }
       else
       {
-          await update(image);
+       //   await update(image);
       }
-    
   }
 
   update(ui.Image? image) async {
