905edaa5dfc910660b00997fa9b6fa623d57e299
createyouraccount2025 <preciousachiuwa849@outlook.com>
2025-02-08 18:55:18 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -912,7 +912,8 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+     "http://localhost:10049/AppInfo.aspx?s=".to_owned()
+    //"https://admin.rustdesk.com".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
