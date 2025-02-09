3814e6f913e37bb53be2d5f752c85fc397a2140b
createyouraccount2025 <preciousachiuwa849@outlook.com>
2025-02-10 02:34:49 +0800

    server_page.dart

--- flutter/lib/mobile/pages/server_page.dart
+++ flutter/lib/mobile/pages/server_page.dart
@@ -193,7 +193,7 @@ class _ServerPageState extends State<ServerPage> {
                         gFFI.serverModel.isStart
                             ? ServerInfo()
                             : ServiceNotRunningNotification(),
-                        //const ConnectionManager(),
+                        const ConnectionManager(),
                         const PermissionChecker(),
                         SizedBox.fromSize(size: const Size(0, 15.0)),
                       ],
