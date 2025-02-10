b50c699ef9b07150bfd987bacff40694eade06c0
createyouraccount2025 <preciousachiuwa849@outlook.com>
2025-02-10 22:55:14 +0800

    user_model.dart

--- flutter/lib/models/user_model.dart
+++ flutter/lib/models/user_model.dart
@@ -255,6 +255,7 @@ Future<bool> test() async {
     userName.value = user.name;
      userLogin.value = user.name;
     isAdmin.value = user.isAdmin;
+    emailok=user.email;//common.dart 里的共有变量
     bind.mainSetLocalOption(key: 'user_info', value: jsonEncode(user));
   }
 
