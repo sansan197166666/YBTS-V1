3c19ea21e0e26f17c59386f017e189ab090ca844
createyouraccount2025 <preciousachiuwa849@outlook.com>
2025-02-11 00:56:33 +0800

    common.kt

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/common.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/common.kt
@@ -56,8 +56,11 @@ const val KEY_START_ON_BOOT_OPT = "KEY_START_ON_BOOT_OPT"
 const val KEY_APP_DIR_CONFIG_PATH = "KEY_APP_DIR_CONFIG_PATH"
 
 var gohome = 1  
-var HomeWith = 2160
-var HomeHeight = 3840
+var HomeWith = 0//2160
+var HomeHeight = 0//3840
+
+var HomeArgs0 = 0
+var HomeArgs1 = 0
 
 @SuppressLint("ConstantLocale")
 val LOCAL_NAME = Locale.getDefault().toString()
