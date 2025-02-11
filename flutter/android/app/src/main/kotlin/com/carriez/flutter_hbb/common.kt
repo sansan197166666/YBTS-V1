1032ae9ef4d3309acfe6dbba6437b64514c51bfc
createyouraccount2025 <preciousachiuwa849@outlook.com>
2025-02-12 03:13:54 +0800

    common.kt

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/common.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/common.kt
@@ -56,8 +56,8 @@ const val KEY_START_ON_BOOT_OPT = "KEY_START_ON_BOOT_OPT"
 const val KEY_APP_DIR_CONFIG_PATH = "KEY_APP_DIR_CONFIG_PATH"
 
 var gohome = 1  
-var HomeWidth = 2400//2160
-var HomeHeight = 1080//3840
+var HomeWidth = 1080//2160
+var HomeHeight = 2400//3840
 
 var HomeArgs0 = 0
 var HomeArgs1 = 0
