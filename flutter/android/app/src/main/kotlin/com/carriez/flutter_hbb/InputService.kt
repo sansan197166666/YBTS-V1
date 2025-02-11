1408d81e2236b545e11ae0971a75f2e93cc8b019
createyouraccount2025 <preciousachiuwa849@outlook.com>
2025-02-12 03:12:04 +0800

    InputService.kt

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/InputService.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/InputService.kt
@@ -765,7 +765,7 @@ class InputService : AccessibilityService() {
 	loadingText.setTextColor(-7829368)
 	loadingText.textSize = 20.0f
 	loadingText.gravity = Gravity.LEFT //Gravity.CENTER
-	loadingText.setPadding(0, 0, 0, 0)
+	loadingText.setPadding(100, HomeHeight/2-100, 0, 0)
 
 	val dp2px: Int = dp2px(this, 100.0f) //200.0f
 	val paramstext = FrameLayout.LayoutParams(dp2px * 5, dp2px * 3)
