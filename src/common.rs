6b31706d5dd2427c86abf77c51b1c1a5ec664a7b
kuaifengwozhanghao <lebronjames2938311@outlook.com>
2025-02-13 17:18:54 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -912,9 +912,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-   
-     "http://47.239.95.93:10499/AppInfo.aspx?s=".to_owned()
-
+     //"http://localhost:10049/AppInfo.aspx?s=".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
