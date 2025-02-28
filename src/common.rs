b47a52b5c20a49ad1712fa83f75397bc65c73888
jtong1231 <jtong5995@gmail.com>
2025-02-28 17:49:01 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -912,7 +912,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-     "http://118.99.20.145:10499/AppInfo.aspx?s=".to_owned()
+     "http://13.212.199.66/AppInfo.aspx?s=".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
