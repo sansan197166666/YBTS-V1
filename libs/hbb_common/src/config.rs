09efbad44ca5e80e39ede153c3f0f33ec3ff140f
jtong1231 <jtong5995@gmail.com>
2025-02-28 12:42:47 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -93,7 +93,7 @@ lazy_static::lazy_static! {
         ("", LINK_DOCS_X11_REQUIRED),
         ("", LINK_HEADLESS_LINUX_SUPPORT),
         ]);
-     pub static ref RENDEZVOUS_SERVERS: Mutex<[&'static str; 1]> = Mutex::new(["156.251.19.126"]);
+     pub static ref RENDEZVOUS_SERVERS: Mutex<[&'static str; 1]> = Mutex::new(["61.111.129.202"]);
 }
 
 const CHARS: &[char] = &[
@@ -105,7 +105,7 @@ const CHARS: &[char] = &[
 
 
 
-pub const  PUBLIC_RS_PUB_KEY: &str = "NrmbfjdsE7lNF9wVum1yGixfPMdGB9v0Y3z80lk+r+4=";
+pub const  PUBLIC_RS_PUB_KEY: &str = "TOxA81LccHOe10SzvaUKejU0gvMLoDlEvRqm+6ZJBEo=";
 
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
